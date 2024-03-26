b4a5218c63c5d235d97b6a1dca2fe36fcb5ea8f7
javimunozz <jpmuoz@miuandes.cl>
2024-03-26 19:18:15 -0300

    Create Y

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+    #class nombres separados por mayuscula
+    #funciones
+    def home 
+    end
+    def about
+    end
+    def contact
+    end
+end
